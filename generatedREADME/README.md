4e420c8742db326ff2679ab20203f1e72e4e500c
cgwol <cgwalterson@icloud.com>
2023-08-12 14:43:32 -0700

    Final test

--- generatedREADME/README.md
+++ generatedREADME/README.md
@@ -1,14 +1,28 @@
-# Testing Link
-![Mozilla Public License 2.0 License](https://img.shields.io/badge/license-Mozilla%20Public%20License%202.0-blue)
+# Final test
+![MIT License](https://img.shields.io/badge/license-MIT-blue)
 
 ## Description 
-  Missing Link
+  final test
 
 ## Table of Contents
   
   - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
   - [License](#license)
+  - [Questions](#questions)
+
+## Installation
+  npm i
+
+## Usage
+  node index.js
 
 ## License
-  This Application is licensed under the [Mozilla Public License 2.0](https://choosealicense.com/licenses/mpl-2.0/) license
+  This Application is licensed under the [MIT](https://choosealicense.com/licenses/mit/) license
+  
+## Questions
+  Created by: [cgwol](github.com/cgwol)
+  
+  Contact with any further questions at [email@email.email](mailto:email@email.email)
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,5 @@
 class Output {
-  constructor(title, desc, install, usage, contributing, license, licenseLink) {
+  constructor(title, desc, install, usage, contributing, license, licenseLink, tests, github, email) {
     this.title = title;
     this.desc = desc;
     this.install = install;
@@ -7,51 +7,11 @@ class Output {
     this.contributing = contributing;
     this.license = license;
     this.licenseLink = licenseLink;
+    this.tests = tests;
+    this.github = github;
+    this.email = email;
   }
 }
-
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function getLicenseBadge(license) {
-  return `![${license} License](https://img.shields.io/badge/license-${license.split(' ').join('%20')}-blue)
-`
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function getLicenseLink(license) {
-  var link;
-  switch (license){
-    case 'MIT':
-      link = `https://choosealicense.com/licenses/mit/`;
-      break;
-    case 'Apache 2.0':
-      link = `https://choosealicense.com/licenses/apache-2.0/`
-      break;
-    case 'GNU AGLPv3':
-      link = `https://choosealicense.com/licenses/agpl-3.0/`
-      break;
-    case 'GNU GPLv3':
-      link = `https://choosealicense.com/licenses/gpl-3.0/`;
-      break;
-    case 'Mozilla Public License 2.0':
-      link = `https://choosealicense.com/licenses/mpl-2.0/`;
-      break;
-    case 'Boost Software License 1.0': 
-      link = `https://choosealicense.com/licenses/bsl-1.0/`;
-      break;
-    default:
-      link = ``;
-      break;
-  }
-
-
-  return link;
-}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) { }
 
 // Function to generate markdown for README
 function generateMarkdown(data) {
@@ -64,6 +24,9 @@ function generateMarkdown(data) {
     contributing = data.contributing,
     license = data.license,
     licenseLink = getLicenseLink(data.license),
+    tests = data.tests,
+    github = data.github,
+    email = data.email
   );
 
   // Optional Input Flags
@@ -80,24 +43,29 @@ function generateMarkdown(data) {
     licenseFlag = true
     // Getting License Badge
     var licenseBadge = getLicenseBadge(output.license);
-    var licenseLink = getLicenseLink(output.license);
   }
-
-
+  var testsFlag = false;
+  if (output.tests != '') { testsFlag = true }
+  var githubFlag = false;
+  if (output.github != '') { githubFlag = true }
+  var emailFlag = false;
+  if (output.email != '') { emailFlag = true } 
 
   // Appending Mark Down Output
   var outputMD = createTitle(output.title);
   if (licenseFlag) { outputMD += licenseBadge }
   if (descFlag) { outputMD += createDesc(output.desc) }
-  if (descFlag || usageFlag || installFlag) { outputMD += createTbl(descFlag, installFlag, usageFlag, contrFlag, licenseFlag); }
+  if (descFlag || usageFlag || installFlag) { outputMD += createTbl(descFlag, installFlag, usageFlag, contrFlag, licenseFlag, testsFlag, githubFlag, emailFlag); }
   if (installFlag) { outputMD += createInstall(output.install) }
   if (usageFlag) { outputMD += createUsage(output.usage) }
   if (contrFlag) { outputMD += createContr(output.contributing) }
   if (licenseFlag) { outputMD += createlicense(output.license, output.licenseLink) }
+  if (testsFlag) { outputMD += createTests(output.tests) }
+  if (githubFlag || emailFlag) { outputMD += createQuestions() }
+  if (githubFlag) { outputMD += createGithub(output.github) }
+  if (emailFlag) { outputMD += createEmail(output.email) }
 
   return outputMD;
-
-
 }
 
 // Create Functions
@@ -106,10 +74,10 @@ function createTitle(data) {
 `
 }
 
-function createTbl(descFlag, installFlag, usageFlag, contrFlag, licenseFlag) {
+function createTbl(descFlag, installFlag, usageFlag, contrFlag, licenseFlag, testsFlag, githubFlag, emailFlag) {
   return `
 ## Table of Contents
-  ` + tblAppends(descFlag, installFlag, usageFlag, contrFlag, licenseFlag);
+  ` + tblAppends(descFlag, installFlag, usageFlag, contrFlag, licenseFlag, testsFlag, githubFlag, emailFlag);
 }
 
 function createDesc(data) {
@@ -147,7 +115,68 @@ function createlicense(data, link) {
   `
 }
 
-function tblAppends(descFlag, installFlag, usageFlag, contrFlag, licenseFlag) {
+function createQuestions(){
+  return `
+## Questions`
+}
+
+function createGithub(data) {
+  return `
+  Created by: [${data}](github.com/${data})
+  `
+}
+
+function createEmail(data){
+  return `
+  Contact with any further questions at [${data}](mailto:${data})
+  `
+}
+
+function createTests(data){
+  return `
+## Testing
+  ${data}
+  `
+}
+
+// Getting License Link
+function getLicenseLink(license) {
+  var link;
+  switch (license){
+    case 'MIT':
+      link = `https://choosealicense.com/licenses/mit/`;
+      break;
+    case 'Apache 2.0':
+      link = `https://choosealicense.com/licenses/apache-2.0/`
+      break;
+    case 'GNU AGLPv3':
+      link = `https://choosealicense.com/licenses/agpl-3.0/`
+      break;
+    case 'GNU GPLv3':
+      link = `https://choosealicense.com/licenses/gpl-3.0/`;
+      break;
+    case 'Mozilla Public License 2.0':
+      link = `https://choosealicense.com/licenses/mpl-2.0/`;
+      break;
+    case 'Boost Software License 1.0': 
+      link = `https://choosealicense.com/licenses/bsl-1.0/`;
+      break;
+    default:
+      link = ``;
+      break;
+  }
+
+  return link;
+}
+
+// Getting License badge
+function getLicenseBadge(license) {
+  return `![${license} License](https://img.shields.io/badge/license-${license.split(' ').join('%20')}-blue)
+`
+}
+
+// Only adds elemnts to the table that werent left empty
+function tblAppends(descFlag, installFlag, usageFlag, contrFlag, licenseFlag, testsFlag, githubFlag, emailFlag) {
   var appends = `
  `;
   if (descFlag) {
@@ -170,9 +199,16 @@ function tblAppends(descFlag, installFlag, usageFlag, contrFlag, licenseFlag) {
     appends += `  - [License](#license)
 `
   }
+  if (testsFlag){
+    appends += `  - [Testing](#testing)
+`
+  }
+  if (githubFlag || emailFlag){
+    appends += `  - [Questions](#questions)
+`
+  }
 
   return appends;
 }
-
 
 module.exports = generateMarkdown;
